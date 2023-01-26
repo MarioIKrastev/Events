bd7276a2d46a090ddd6e38ed3d7ec23e652dc14e
Mario <mariookrastev@gmail.com>
2023-01-26 16:12:05 +0200

    added

--- server/dist/events/events.controller.d.ts
+++ server/dist/events/events.controller.d.ts
@@ -1,12 +1,12 @@
-import ReqWithUser from 'src/common/interfaces/ReqWithUser.interface';
+import { User } from 'src/typeorm';
 import { EventDto } from './dto';
 import { EventsService } from './events.service';
 export declare class EventsController {
     private eventsService;
     constructor(eventsService: EventsService);
-    postEvent(dto: EventDto, req: ReqWithUser): Promise<import("src/typeorm").Event>;
+    postEvent(dto: EventDto, user: User): Promise<import("src/typeorm").Event>;
     getAll(): Promise<import("src/typeorm").Event[]>;
     findOne(id: string): string;
     update(id: string, dto: EventDto): string;
-    remove(id: string, res: any): Promise<import("typeorm").DeleteResult>;
+    remove(id: string): Promise<import("typeorm").DeleteResult>;
 }
--- server/dist/events/events.controller.js
+++ server/dist/events/events.controller.js
@@ -15,15 +15,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.EventsController = void 0;
 const common_1 = require("@nestjs/common");
 const decorators_1 = require("../common/decorators");
-const jwtAuthorization_guard_1 = require("../common/guards/jwtAuthorization.guard");
+const typeorm_1 = require("../typeorm");
 const dto_1 = require("./dto");
 const events_service_1 = require("./events.service");
 let EventsController = class EventsController {
     constructor(eventsService) {
         this.eventsService = eventsService;
     }
-    postEvent(dto, req) {
-        return this.eventsService.postEvent(dto, req.user);
+    postEvent(dto, user) {
+        return this.eventsService.postEvent(dto, user);
     }
     getAll() {
         return this.eventsService.getAll();
@@ -34,31 +34,22 @@ let EventsController = class EventsController {
     update(id, dto) {
         return this.eventsService.update(+id, dto);
     }
-    remove(id, res) {
-        try {
-            res.status(200).json('Deleted successfully');
-            return this.eventsService.remove(+id);
-        }
-        catch (error) {
-            console.log(error);
-        }
+    remove(id) {
+        return this.eventsService.remove(+id);
     }
 };
 __decorate([
     (0, decorators_1.Public)(),
     (0, common_1.Post)('add'),
-    (0, common_1.UseGuards)(jwtAuthorization_guard_1.default),
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     __param(0, (0, common_1.Body)()),
-    __param(1, (0, common_1.Req)()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [dto_1.EventDto, Object]),
+    __metadata("design:paramtypes", [dto_1.EventDto, typeorm_1.User]),
     __metadata("design:returntype", void 0)
 ], EventsController.prototype, "postEvent", null);
 __decorate([
     (0, decorators_1.Public)(),
-    (0, common_1.Get)(),
-    (0, common_1.UseGuards)(jwtAuthorization_guard_1.default),
+    (0, common_1.Get)('events'),
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
@@ -80,13 +71,10 @@ __decorate([
     __metadata("design:returntype", void 0)
 ], EventsController.prototype, "update", null);
 __decorate([
-    (0, common_1.UseGuards)(jwtAuthorization_guard_1.default),
-    (0, common_1.HttpCode)(common_1.HttpStatus.OK),
     (0, common_1.Delete)(':id'),
     __param(0, (0, common_1.Param)('id')),
-    __param(1, (0, common_1.Res)()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [String, Object]),
+    __metadata("design:paramtypes", [String]),
     __metadata("design:returntype", void 0)
 ], EventsController.prototype, "remove", null);
 EventsController = __decorate([
